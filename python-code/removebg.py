e98f63f9d9650e7399e9d2c2c7fa1e4b1db6675d
nuggy875 <jinugpaeng@gmail.com>
2019-09-25 21:54:00 +0900

    algoritalgorithm mode 3 and 4

--- python-code/removebg.py
+++ python-code/removebg.py
@@ -39,8 +39,8 @@ def findContour(input_img, output_img):
     #-- Find contours in edges, sort by area ---------------------------------------------
     contour_info = []
     # Previously, for a previous version of cv2, this line was: 
-    # _, contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
-    contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    _, contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
+    #contours, _ = cv2.findContours(edges, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
     # Thanks to notes from commenters, I've updated the code but left this note
     for c in contours:
         contour_info.append((
@@ -75,8 +75,8 @@ def findContour(input_img, output_img):
     rgba = [b,g,r, alpha]
     dst = cv2.merge(rgba,4)
 
-    cv2.imshow('img', dst)                                   # Display
-    cv2.waitKey()
+    #cv2.imshow('img', dst)                                   # Display
+    #cv2.waitKey()
     cv2.imwrite(output_img, dst)           # Save
 
 
@@ -101,10 +101,61 @@ def grabCut(input_img, output_img):
     rgba = [b,g,r, alpha]
     dst = cv2.merge(rgba,4)
 
-    cv2.imshow('img', dst)                                   # Display
-    cv2.waitKey()
+    #cv2.imshow('img', dst)                                   # Display
+    #cv2.waitKey()
     cv2.imwrite(output_img, dst)           # Save
 
+
+def watershed(input_img, output_img):
+    img = cv2.imread(input_img)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    
+    #Morphology의 opening, closing을 통해서 노이즈나 Hole제거
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel,iterations=2)
+
+    # dilate를 통해서 확실한 Backgroud
+    sure_bg = cv2.dilate(opening,kernel,iterations=3)
+
+    #distance transform을 적용하면 중심으로 부터 Skeleton Image를 얻을 수 있음.
+    # 즉, 중심으로 부터 점점 옅어져 가는 영상.
+    # 그 결과에 thresh를 이용하여 확실한 FG를 파악
+    dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+    ret, sure_fg = cv2.threshold(dist_transform,0.5*dist_transform.max(),255,0)
+    sure_fg = np.uint8(sure_fg)
+
+    # Background에서 Foregrand를 제외한 영역을 Unknow영역으로 파악
+    unknown = cv2.subtract(sure_bg, sure_fg)
+
+    # FG에 Labelling작업
+    ret, markers = cv2.connectedComponents(sure_fg)
+    markers = markers + 1
+    markers[unknown == 255] = 0
+
+    # watershed를 적용하고 경계 영역에 색지정
+    markers = cv2.watershed(img,markers)
+    img[markers == -1] = [255,0,0]
+
+    images = [gray, thresh, sure_bg, dist_transform, sure_fg, unknown, markers, img]
+    titles = ['Gray','Binary','Sure BG','Distance','Sure FG','Unknow','Markers','Result']
+
+    for i in range(len(images)):
+        plt.subplot(2, 4, i+1), plt.imshow(images[i]), plt.title(titles[i]), plt.xticks([]), plt.yticks([])
+
+    # plt.show()
+    cv2.imwrite(output_img, img)           # Save
+
+def bgSubMOG2(input_img, output_img):
+    fgbg = cv2.createBackgroundSubtractorMOG2()
+    img = cv2.imread(input_img)
+    fgmask = fgbg.apply(img)
+    #cv2.imshow('fgmask', img)
+    #cv2.waitKey()
+
+    #cv2.imshow('frame', fgmask)
+    #cv2.waitKey()
+    cv2.imwrite(output_img, fgmask)           # Save
 
 
 # python removebg.py 1 test5
@@ -120,16 +171,25 @@ if __name__ == "__main__":
     det_path = osp.realpath('../public/result')
     if not os.path.exists(det_path):
         os.makedirs(det_path)
-    output_img_path = osp.join(det_path, sys.argv[2]+'_'+sys.argv[1]+'_rst.png')
+    output_img_path = osp.join(det_path, sys.argv[1]+'_'+sys.argv[2]+'_rst.png')
 
     if not os.path.exists(input_img_path):
         print('ERROR:ImageNotExists')
         exit()
     
+    # findConcour 알고리즘 (OpenCV)
     if sys.argv[1] == '1':
         findContour(input_img_path, output_img_path)
+    # grapcut 알고리즘 (OpenCV)
     elif sys.argv[1] == '2':
         grabCut(input_img_path, output_img_path)
+    # watershed 알고리즘 (OpenCV)
+    elif sys.argv[1] == '3':
+        watershed(input_img_path, output_img_path)
+    # BackgroundSubtractorMOG2 알고리즘 (OpenCV)
+    elif sys.argv[1] == '4':
+        bgSubMOG2(input_img_path, output_img_path)
+    
     else:
         print('else')
         
