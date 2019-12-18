d2a1826a6932667d506728e97fca06189738b840
nuggy875 <jinugpaeng@gmail.com>
2019-12-18 15:46:18 +0000

    test code complete

---
+++ python-code/imageMatting_test.py
@@ -0,0 +1,10 @@
+import cv2
+import os
+import numpy as np
+
+if __name__ == '__main__':
+    cv2.imread(os.path.abspath(os.path.join(os.path.dirname(__file__), "../public/input/input.jpg")))
+    cv2.imread(os.path.abspath(os.path.join(os.path.dirname(__file__), "../public/input/input_a.jpg")))
+    img_test = np.zeros((500, 500, 3), np.uint8)
+    cv2.imwrite(os.path.abspath(os.path.join(os.path.dirname(__file__), "../public/result/7_compose.jpg")), img_test)
+    print('connected2')
